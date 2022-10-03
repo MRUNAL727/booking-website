3456c7bd4779719cb63cc77a32d882b17dd64615
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-03 14:21:04 +0530

    ui

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './navbar.css'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="navContainer"></div>
+        <div className="navContainer">
+          <span className='logo'>Booking</span>
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        </div>
     </div>
   )
 }
