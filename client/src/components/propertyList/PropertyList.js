6f34036e9aeb08f0605da6fbd24fd2873b1fc13a
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-07 19:41:57 +0530

    ui

--- client/src/components/propertyList/PropertyList.js
+++ client/src/components/propertyList/PropertyList.js
@@ -20,7 +20,7 @@ const PropertyList = () => {
       <div className="pListItem">
         <img
           src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
-          alt="" 
+          alt=""  
           className="pListImg"
         />
         <div className="pListTitles">
