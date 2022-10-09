1964409da0c8bc2822d116b83e4cc5565cfc4b69
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-09 09:26:34 +0530

    server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
@@ -19,6 +18,10 @@ const connect = async () => {
     throw error;
   }
 };
+
+//middlewares
+app.use(cors())
+app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
