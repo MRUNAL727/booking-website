9cc07fc0a3b15e1a5a429ef4aa51aa6971734961
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-11 10:04:20 +0530

    server

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -6,12 +6,14 @@ export const verifyToken = async (req, res, next) => {
 
   if (!token) {
     return next(createError(401, 'You are not authenticated'));
-  } else {
+  } 
+  else {
     jwt.verify(token, process.env.JWT_SECRETE, (err, user) => {
       if (err) {
         return next(createError(403, 'Invalid token'));
       } else {
         req.user = user;
+      console.log('one')
         next();
       }
     });
@@ -19,8 +21,13 @@ export const verifyToken = async (req, res, next) => {
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  console.log("hey");
+  verifyToken(req, res, () => {
+      console.log('two')
+
     if (req.user.id === req.params.id || req.user.isAdmin) {
+      console.log('three')
+
       next();
     } else {
       return next(createError(403, "You are not authorized!"));
