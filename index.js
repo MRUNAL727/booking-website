0daf8ced3efdde93c602732b3fd67f18a1c35df5
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-12-02 10:39:25 +0530

    production

--- index.js
+++ index.js
@@ -36,15 +36,15 @@ const connect = async () => {
 // ));
 
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Content-Type', 'application/json;charset=UTF-8');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Content-Type', 'application/json;charset=UTF-8');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 app.use(express.json());
 app.use(cookieParser());
 
